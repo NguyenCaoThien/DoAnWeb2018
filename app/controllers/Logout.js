34416216ba2800a0093c5db88b0653430a8df543
Thien-PC\Thien <nguyenthien250797@gmail.com>
2018-06-26 22:14:33 +0700

    update giao dien

--- app/controllers/Logout.js
+++
@@ -1,8 +0,0 @@
-var LoginController = require('./LogIn')
-var express = require("express")
-var router = express.Router()
-
-
-router.get('/',LoginController.adminLogout)
-
-module.exports = router;
--- public/MuonTra.js
+++
@@ -1,8 +0,0 @@
-      $(function(){
-        $('.delete').click(function(e) {
-          e.preventDefault();
-          $('#result').val($(this).closest('tr').find('td:first').text());
-          $('#result1').val($(this).closest('tr').find('td:nth-child(3)').text());
-          $('#result2').val($(this).closest('tr').find('td:nth-child(8)').text());
-        });
-      });
